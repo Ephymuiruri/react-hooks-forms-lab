2f7b9d76708feb76c3e785714306e95ef5973d2b
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-29 10:34:02 +0300

    search functionality working

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,25 +5,48 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-
+  const [searchItem, setSearchItem] = useState("");
+  const [ filteredItems,setfilteredItems] = useState([])
+  const [display, setDisplay] = useState([]);
+console.log(filteredItems)
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchItem(event) {
+    const searchValue = event.target.value; 
+    setSearchItem(searchValue); 
+    searchfilter(searchValue)
+    return searchValue
+  }
+  function searchfilter(searchValue){
+      const searcharray = items.filter(
+      (item) => item.name.toLowerCase().includes(searchValue.toLowerCase()));
+      setfilteredItems(searcharray)
+      if (searcharray.length!==0){setDisplay(searcharray)}
+    console.log(searcharray)
+  }
+      
+  const categoryDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
-
+  
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} 
+      searchItem={searchItem} 
+      onSearch={handleSearchItem} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {display.length === 0 ? 
+              categoryDisplay.map((item) => (
+              <Item key={item.id} name={item.name} category={item.category} />
+            )):display.map((item) => (
+              <Item key={item.id} name={item.name} category={item.category} />
+            ))
+            }
       </ul>
     </div>
   );
